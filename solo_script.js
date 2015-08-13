49150a1a3e5624bfc70570b330f896f51ca7d9ca
kstoffregen <stoffrk@Kathryns-MacBook-Pro.local>
2015-08-13 09:17:53 -0500

    first commit

---
+++ solo_script.js
@@ -0,0 +1,88 @@
+// ! ! !
+// Three Bugs
+// 1. Changed <array[i] = calculateSTI(array);> to <array[i] = calculateSTI(array[i]);> in for loop
+// 2. Changed <return basePercent - 1;> to <return basePercent;> in getBaseSTI function
+// 3. Added Math.round() to <newArray[2] = baseSalary * (1.0 + bonus);> and <newArray[3] = baseSalary * bonus;>
+// 4. Added .join(', ') to end of <array[i] = calculateSTI(array[i]);> to add space between elements
+
+var arrayAtticus = ["Atticus", "2405", "47000", 3];
+var arrayJem = ["Jem", "62347", "63500", 4];
+var arrayBoo = ["Boo", "11435", "54000", 3];
+var arrayScout = ["Scout", "6243", "74750", 5];
+
+var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+
+//Create variables used to write to the DOM
+var newEl, newText, position;
+//Capture the position of insertion into the DOM
+position = document.getElementById('content');
+
+//Loop the array, extracting each array and writing information to the DOM
+//Note that the information is not 'clean'
+for(var i = 0; i < array.length; i++){
+  array[i] = calculateSTI(array[i]).join(', ');
+  newEl = document.createElement('li');
+  newText = document.createTextNode(array[i]);
+  newEl.appendChild(newText);
+  position.appendChild(newEl);
+}
+
+function calculateSTI(array){
+  var newArray = [];
+
+  newArray[0] = array[0];
+
+  var employeeNumber = array[1];
+  var baseSalary = array[2];
+  var reviewScore = array[3];
+
+  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+  if(bonus > 0.13){
+    bonus = 0.13;
+  }
+
+  newArray[1] = bonus;
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus));
+  newArray[3] = Math.round(baseSalary * bonus);
+  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
+  return newArray;
+}
+
+function getBaseSTI(reviewScore){
+  var basePercent;
+  switch(reviewScore){
+    case 1:
+      basePercent = 0;
+      break;
+    case 2:
+      basePercent = 0;
+      break;
+    case 3:
+      basePercent = 0.04;
+      break;
+    case 4:
+      basePercent = 0.06;
+      break;
+    case 5:
+      basePercent = 0.10;
+      break;
+  }
+  return basePercent; //- 1;
+}
+
+function getYearAdjustment(employeeNumber){
+  var yearAdjustment = 0;
+  if(employeeNumber.length == 4){
+    yearAdjustment = 0.05;
+  }
+  return yearAdjustment;
+}
+
+function getIncomeAdjustment(salary){
+  var incomeAdjustment = 0;
+  salary = parseInt(salary);
+  if(salary > 65000){
+    incomeAdjustment = 0.01;
+  }
+  return incomeAdjustment;
+}
